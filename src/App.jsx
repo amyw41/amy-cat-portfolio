5836e1345f55a35c9b54bb4f827f4e92134c9c04
Amy Wang <amysonothere@gmail.com>
2026-03-30 14:59:47 -0400

    initial commit - cat portfolio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import mainCat from './assets/main-cat.png'
+
+function App() {
+  return (
+    <div className="app">
+      <img src={mainCat} alt="main-cat" />
+    </div>
+  )
+}
+
+export default App
